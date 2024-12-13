8fc2dc019d7dacc63c0b145b7d43c015f61b3b24
bestes83 <bestes83@gmail.com>
2024-12-13 09:52:02 -0600

    removed old projects. to use a differenct architecture pattern.

--- Pokemon.NextJS/client/config.ts
+++
@@ -1,5 +0,0 @@
-export abstract class Config {
-    public static PokemonThumbnailUrl:string = "https://raw.githubusercontent.com/bestes83/PokemonAssets/master/assets/thumbnails/";
-    public static PokemonImageUrl:string = "https://raw.githubusercontent.com/bestes83/PokemonAssets/master/assets/images/"
-    public static PokemonApi:string = "http://localhost:5250/api";
-}
--- Pokemon.NextJS/client/models/pokemonModel.ts
+++
@@ -1,9 +0,0 @@
-export interface PokemonModel{
-    name: string;
-    id:string;
-    types:string[];
-    heightInMeters: number;
-    weightInKg: number;
-    genus:string;
-    abilities:string[]
-}
